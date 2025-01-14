a7cd0715214c2240813dcb180ac5e0a1ee1f57eb
Sebastián Fuentes <ssfuentes.99@gmail.com>
2025-01-14 18:51:51 -0500

    xxx

--- src/Services/HttpClient.ts
+++ src/Services/HttpClient.ts
@@ -5,7 +5,7 @@ const httpClient = axios.create({
     baseURL: "https://deployrailwayapi-production.up.railway.app/api", // Define la URL base
     timeout: 5000, // Tiempo máximo de espera
     headers: {
-        "Authorization": "Bearer" + localStorage.getItem("token"), // Token de autenticación
+        "Authorization": "Bearer" + "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJnaXZlbl9uYW1lIjoiYWRtaW4iLCJ1c2VySWQiOiIxMTExMTExMS0xMTExLTExMTEtMTExMS0xMTExMTExMTExMTEiLCJuYmYiOjE3MzY4OTg0ODIsImV4cCI6MTczNzUwMzI4MiwiaWF0IjoxNzM2ODk4NDgyLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo1MDAxIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMSJ9._QbVqCAim-Hx24VXOBrIUY6e8tdnFvWjP88OgtbFDv-whSA1iD9fp07h-ZiPTm9UrIuLKU-9HDntttelzqFkJw", // Token de autenticación
     }
 });
 
